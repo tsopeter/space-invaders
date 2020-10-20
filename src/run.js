1212b0b50458c18aabb896f614f335cae2cca113
Tso, Peter <tsopeter@ku.edu>
2020-10-20 16:30:22 -0500

    Changed Comments

--- src/run.js
+++ src/run.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	//
 	//got the idea to have a Promise.all to check array of
 	//promises from user
-	//https://stackoverflow.com/questions/11071314/javascript-execute-after-all
-	//-images-have-loaded
+	//https://stackoverflow.com/questions/11071314/javascript-execute-after-all-images-have-loaded
 
 	//
 	//Promise that all title images have been loaded
