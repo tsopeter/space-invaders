0b47ef79b0115eb1a57e5da0f2470b221215ed9b
Tso, Peter <tsopeter@ku.edu>
2020-10-23 00:02:32 -0500

    Removed Comment Block

--- src/render.js
+++ src/render.js
@@ -5,21 +5,6 @@ function render(){
 	//draw the background
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	//
-	//get the gameBoard objects and render onto screen
-//	for(var i = 0; i < gameBoard.height; i++){
-//		for(var k = 0; k < gameBoard.width; k++){
-//			if(typeof(gameBoard.returnObj(k, i)) != 'undefined'){
-//				//get object
-//				let obj = gameBoard.returnObj(k , i);
-//			
-//				//
-//				//function from resource.js
-//				draw(obj.sprite, scale * obj.x, scale * obj.y);
-//			}
-//		}
-//	}
 	
 	for(var i = 0; i < objArr.length; i++){
 		let o = objArr[i];
